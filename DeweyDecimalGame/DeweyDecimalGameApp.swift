99c96d245f2dea50d04b04731bc9a4376127930d
Leo Lai <scipio1516@gmail.com>
2023-01-24 09:28:38 -0600

    Initial Commit

---
+++ DeweyDecimalGame/DeweyDecimalGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeweyDecimalGameApp.swift
+//  DeweyDecimalGame
+//
+//  Created by Leo Lai on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct DeweyDecimalGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
