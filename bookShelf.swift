7aea5892c2eb03b943ea499282c97cdcb4056977
Jack <Jwenschhof@bsd220.org>
2023-02-16 09:15:42 -0600

    Creation of the bookshelf view.

---
+++ bookShelf.swift
@@ -0,0 +1,34 @@
+//
+//  bookShelf.swift
+//  DeweyDecimalGame
+//
+//  Created by Jack W on 2/16/23.
+//
+
+import SwiftUI
+
+struct bookShelf: View {
+    var body: some View {
+        VStack(spacing: 40) {
+            VStack(spacing: 20) {
+                Rectangle()
+                    .frame(width: 350, height: 700)
+                    .foregroundColor(Color.brown.opacity(0.0))
+                    .overlay(RoundedRectangle(cornerRadius: 3).stroke(Color.brown, lineWidth: 3))
+                Rectangle()
+                    .frame(width: 1, height: 2)
+                    .foregroundColor(Color.brown.opacity(0.0))
+                    .overlay(RoundedRectangle(cornerRadius: 3).stroke(Color.brown, lineWidth: 3))
+                
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .ignoresSafeArea(.all).background(Color(hue: 0.599, saturation: 0.433, brightness: 0.971))
+    }
+}
+
+struct bookShelf_Previews: PreviewProvider {
+    static var previews: some View {
+        bookShelf()
+    }
+}
