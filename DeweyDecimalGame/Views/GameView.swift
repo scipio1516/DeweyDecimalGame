9248b20ef9a7ae091edca5caf393cfdf17a12335
Leo Lai <scipio1516@gmail.com>
2023-03-01 09:25:22 -0600

    Level Select created

--- DeweyDecimalGame/Views/GameView.swift
+++ DeweyDecimalGame/Views/GameView.swift
@@ -13,57 +13,64 @@ var extraColor = ColorCodable(id: UUID(), red: 0.55, green: 0.35, blue: 0.67)
 
 struct GameView: View {
     
-    @State var bookShelf = [Book(id: UUID(), bookTitle: "BookOne", bookColor: redColor, authorName: "Author One", callID: "AUT", deweyDecimalNumber: "33.333"), Book(id: UUID(), bookTitle: "BookTwo", bookColor: greenColor, authorName: "Author Two", callID: "AUT", deweyDecimalNumber: "23.333"), Book(id: UUID(), bookTitle: "BookThree", bookColor: extraColor, authorName: "Author Three", callID: "AUT", deweyDecimalNumber: "13.333")]
+    @State var bookShelf: [Book]
     @State var isInOrder = false
     var body: some View {
         let bookInfo = BookData(bookArray: bookShelf)
-        VStack {
-            //Currently: Books are a custom view, taking from a custom struct, and have the ondrag property carrying their data. NOTE: does not work in preview, does work in simulator.
-            
-            //drag and drop implemented through a dropDestination modifier/View thingy. works!
-            
-            Button("Check?") {
-                isInOrder = bookInfo.checkForAlphabeticalOrder()
+        
+        HStack {
+            VStack {
+                Button("Check?") {
+                    isInOrder = bookInfo.checkForAlphabeticalOrder()
+                }
+                
+                if(isInOrder) {
+                    Text("Books are in alphabetic order by author name!")
+                        .font(.subheadline)
+                } else {
+                    Text("Books are not in order.")
+                        .font(.subheadline)
+                }
+                
             }
-            
-            if(isInOrder) {
-                Text("Books are in alphabetic order by author name!")
-                    .font(.title)
-            } else {
-                Text("Books are not in order.")
-                    .font(.title)
+            Spacer().frame(width: 50)
+            VStack {
+                //Currently: Books are a custom view, taking from a custom struct, and have the ondrag property carrying their data. NOTE: does not work in preview, does work in simulator.
+                
+                //drag and drop implemented through a dropDestination modifier/View thingy. works!
+                ForEach(bookShelf) { shelf in
+                    BookView(data: shelf)
+                        .dropDestination(for: Book.self) {tempBook, location in
+                            
+                            //currently searching through the bookshelf to get locations, might want a more efficient way of keeping track?
+                            let previousLocation = bookShelf.firstIndex(where: {anotherBook in
+                                return tempBook[0] == anotherBook
+                            })
+                            
+                            let newLocation = (bookShelf.firstIndex(where: { newBookLocation in
+                                return shelf == newBookLocation
+                            }) ?? previousLocation)
+                            bookShelf.remove(at: previousLocation ?? 0)
+                            bookShelf.insert(tempBook[0], at: newLocation ?? 0)
+                            
+                            return true
+                        }
+                }
+                
+                
+                
+                //start with only three books, and try to get ordering working there. Save additional for later
+                
             }
-            ForEach(bookShelf) { shelf in
-                BookView(data: shelf)
-                    .dropDestination(for: Book.self) {tempBook, location in
-                        
-                        //currently searching through the bookshelf to get locations, might want a more efficient way of keeping track?
-                        let previousLocation = bookShelf.firstIndex(where: {anotherBook in
-                            return tempBook[0] == anotherBook
-                        })
-                        
-                        let newLocation = (bookShelf.firstIndex(where: { newBookLocation in
-                            return shelf == newBookLocation
-                        }) ?? previousLocation)
-                        bookShelf.remove(at: previousLocation ?? 0)
-                        bookShelf.insert(tempBook[0], at: newLocation ?? 0)
-                        
-                        return true
-                    }
-            }
+            .rotationEffect(Angle(degrees: -90))
             
-            
-            
-            //start with only three books, and try to get ordering working there. Save additional for later
-            
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .ignoresSafeArea(.all).background(Color(hue: 0.599, saturation: 0.433, brightness: 0.971))
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .ignoresSafeArea(.all).background(Color(hue: 0.599, saturation: 0.433, brightness: 0.971))
     }
 }
 
 struct GameView_Previews: PreviewProvider {
     static var previews: some View {
-        GameView()
+        GameView(bookShelf: [Book(id: UUID(), bookTitle: "BookOne", bookColor: redColor, authorName: "Author One", callID: "AUT", deweyDecimalNumber: "33.333"), Book(id: UUID(), bookTitle: "BookTwo", bookColor: greenColor, authorName: "Author Two", callID: "AUT", deweyDecimalNumber: "23.333"), Book(id: UUID(), bookTitle: "BookThree", bookColor: extraColor, authorName: "Author Three", callID: "AUT", deweyDecimalNumber: "13.333")])
     }
 }
